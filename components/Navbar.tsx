439a39e1cd0c89385508655818a5a9e2e2d66971
Siddharth <preparation013@gmail.com>
2025-04-25 22:59:51 +0530

    merged project

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -36,7 +36,7 @@ function Navbar() {
             </div>
             <div>
               <Button onClick={() => {
-                router.push('/signin')
+                router.push('https://minimannveda-git-main-aditya-rukadikars-projects.vercel.app')
               }} className="hidden md:block bg-[#4A72FF] hover:bg-blue-500 shadow-md shadow-blue-600">
                 Login
               </Button>
